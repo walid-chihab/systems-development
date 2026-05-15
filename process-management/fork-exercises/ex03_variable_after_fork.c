a570f80f3c7176343defce6256cd8b46f1b9a5fe
walid-chihab <its.n.chihab@gmail.com>
2026-05-15 03:06:17 +0100

    -_-

--- process-management/fork-exercises/ex03_variable_after_fork.c
+++ process-management/fork-exercises/ex03_variable_after_fork.c
@@ -1,2 +1,28 @@
 /* ex03_variable_after_fork.c */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int main(){
+    pid_t pid;
+    int n=0;
+    pid=fork();
+    if(pid==-1){
+        perror("fork");
+        exit(0);
+    }
+    if (pid==0){
+        n=n+2;
+        printf("Fils n=%d \n",n);
+    }
+    else {
+        n=n+3;
+        printf("Père n=%d \n",n);
+    }
+    //les deux processus vont faire n=n+1 et afficher n
+    n=n+1;
+    printf("n=%d \n",n);
+    return 0;
+}
--- process-management/fork-exercises/ex04_parallel_execution.c
+++ process-management/fork-exercises/ex04_parallel_execution.c
@@ -1,2 +1,32 @@
 /* ex04_parallel_execution.c */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+int main(){
+    pid_t pid;
+    
+    pid=fork();
+    if(pid==-1){
+        perror("fork");
+        exit(0);
+    }
+    if (pid==0){
+        for (int i=0; i<101; i+=2){
+            printf("%d ,",i);
+        }
+        printf ("\n le fils a terminé d'afficher les nombres paire \n");
+    }
+    else {
+        wait(NULL);
+        for(int i=1; i<100; i+=2){
+            printf("%d ,",i);
+        }
+        printf ("\n le père a terminé d'afficher les nombres impaire \n");
+        printf("ma3lina hayd 3lia \n");
+    }
+    
+    return 0;
+} 
