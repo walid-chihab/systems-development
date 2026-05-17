be210ca57ba97403899c6f057ebcd6e6f4a36e7a
walid-chihab <its.n.chihab@gmail.com>
2026-05-17 03:54:47 +0100

    signaux

--- signals/ex01_ignore_sigint/ignore_sigint.c
+++ signals/ex01_ignore_sigint/ignore_sigint.c
@@ -1,2 +1,26 @@
 /* ignore_sigint.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[]){//
+    if (argc != 2) {//
+        printf("Usage : %s <secondes>\n", argv[0]);//
+        return 1; //
+    }
+
+    int secondes = atoi(argv[1]);// Convertir l'argument en entier  
+    
+    signal (SIGINT, SIG_IGN);
+    
+    sleep (secondes);
+
+    signal (SIGINT, SIG_DFL);
+
+     while (1) ;// Attente infinie pour tester le comportement après avoir rétabli le signal par défaut
+
+    return 0;
+    //si vous appuyez sur Ctrl+C pendant l'exécution du programme, il ignorera le signal SIGINT pendant la période.
+    //apres la period de someile,si vous appuyez à nouveau sur Ctrl+C, le programme se terminera normalement car le signal SIGINT aura été rétabli à son comportement par défaut.
+}
