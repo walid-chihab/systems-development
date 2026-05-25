16d558d8fd8046d127dc1da1dd0c0f42369ccb89
walid-chihab <its.n.chihab@gmail.com>
2026-05-25 09:18:57 +0100

    niiiiiiiiice

--- shared-memory/ex01_message_communication/P1_sender.c
+++ shared-memory/ex01_message_communication/P1_sender.c
@@ -1,2 +1,35 @@
 /* P1_sender.c */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main(int argc, char *argv[]) {
+
+    // vérifier si un message est passé en paramètre
+    if (argc < 2) {
+        printf("Usage: ./P1 <message>\n");
+        exit(1);
+    }
+    key_t key = 1234 ;
+
+    //creation du memoire partager 
+    int  shmid = shmget( key , 1024 ,IPC_CREAT | 0666);
+
+    //attacher au proccesus P1
+     char *msg = (char *) shmat (shmid , NULL , 0);
+
+     //copier le message passer en parametre dans la memoire partager 
+     strcpy(msg ,argv[1]);
+
+     //afficher la confirmation de l'envoi de messsage
+     printf ("p1 envois : %s ", msg);
+
+     //detacher la memoire 
+     shmdt(msg);
+
+     return 0;
+     
+}
--- shared-memory/ex01_message_communication/P2_receiver.c
+++ shared-memory/ex01_message_communication/P2_receiver.c
@@ -1,2 +1,37 @@
 /* P2_receiver.c */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+    //il faut la meme clee que p1 (tres important )
+    key_t key = 1234;
+
+    //recuperer la memoire partager par p1
+    int shmid = shmget(key ,1024, 0666);
+
+    //attacher la memoire au processus p2
+    char msg* = (char *) shmat(shmid , NULL , 0) ;
+
+    //afficher le message recu 
+    printf("p2 a recu : %s\n " ,msg);
+
+    // // convertir en majuscules
+    for (int i = 0; msg[i] != '\0'; i++) {
+        msg[i] = toupper(msg[i]);
+    }
+
+    // afficher en majuscules
+    printf("P2 en majuscules : %s\n", msg);
+
+     // détacher la mémoire
+    shmdt(msg);
+
+    // // supprimer la mémoire (important à la fin)
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
