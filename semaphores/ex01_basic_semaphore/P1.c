70da4d4d23c337443c16da9debdba8d8b5607de5
walid-chihab <its.n.chihab@gmail.com>
2026-06-03 15:43:17 +0100

    70%

--- semaphores/ex01_basic_semaphore/P1.c
+++ semaphores/ex01_basic_semaphore/P1.c
@@ -1,15 +1,20 @@
 /* P1.c */
 
-#include"semaphore.h"
-#define key1 200
-main(){
-int S,n=0;
+#include"semaphore.h"// #include"sem.h" 
+#define key1 200 //
+int main(){ 
+    // S est un sémaphore binaire, initialisé à 0
+    int S,n=0;
 
-S=CREAT_SEM(key1);
-do{ n=n+2;
-printf("%d\t", n);
-}while(n%5!=0);
+    // Créer le sémaphore S avec la clé key1 
+    S=CREAT_SEM(key1);
 
-V(S);
+    do{ 
+        n=n+2;
+        printf("%d\t", n);
+    }while(n%5!=0);
 
+    // V(S) : libérer le sémaphore S
+    V(S);
+    return 0;
 }
--- semaphores/ex01_basic_semaphore/P2.c
+++ semaphores/ex01_basic_semaphore/P2.c
@@ -2,12 +2,17 @@
 
 #include"semaphore.h"
 #define key1 200
-main(){
-int S, n=0;
-S=CREAT_SEM(key1);
-INIT_SEM(S,0);
-P(S);
-do{ n=n+3;
-printf("%d\t", n);
-}while(n%5!=0);
+intmain(){
+    int S, n=0;
+    S=CREAT_SEM(key1);
+    // Initialiser le sémaphore S à 0
+    INIT_SEM(S,0);
+    // P(S) : attendre que le sémaphore S soit libéré
+    P(S);
+    do{ 
+        n=n+3;
+        printf("%d\t", n);
+    }while(n%5!=0);
+    // c pas grave de preparer n pour le p2 , car son deux processus separer chaqun a sa propre variable n
+    return 0;
 }
